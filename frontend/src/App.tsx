7e3d730a97e909f15be7ed40ac3a690c052c7490
Bisho silwal <aayussilwal@gmail.com>
2024-07-11 18:11:48 -0500

    react router dom setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import GlobalContextProvider from "./data/GlobalContext";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: <div>User Profile</div>,
+  },
+]);
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <GlobalContextProvider>
+      <RouterProvider router={router} />
+    </GlobalContextProvider>
+  );
 }
 
 export default App;
